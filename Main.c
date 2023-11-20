aeb8e6709c9f73f07ce6b467c90f6c4653e00135
André Antônio da Silva Queiroz <andreantoniodasilvaqueiroz@gmail.com>
2023-11-20 14:52:42 -0300

    Update Main.c
    deixei formatado o cabeçalho na parte do RA do Gabriel

--- Main.c
+++ Main.c
@@ -3,7 +3,7 @@ Trabalho Final da Disciplina de Algoritmos 01: Gerenciador de PetShop.
 Turma: EC41A.2023_01.C11 / Data:18/04/2023.
 Nomes:André Antônio da Silva Queiroz    | RA:a2575310
 ......Mathias Vinicius Carcano Ferretti | RA:a2575396
-......Gabriel Henrique Prado Affonso | RA:a2618044
+......Gabriel Henrique Prado Affonso    | RA:a2618044
 */
 #include <math.h>
 #include <string.h>
