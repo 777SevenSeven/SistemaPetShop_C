230b175d2e01e0a6fe96ae47a60fc84cfb76c458
mhixy <151560364+mhixy@users.noreply.github.com>
2023-11-20 14:33:58 -0300

    Update Main.c
    os typedef struct, olha se a convencao de nome presta ou preferem _minusculo

--- Main.c
+++ Main.c
@@ -4,6 +4,34 @@
 #include <stdlib.h>
 
 //Type def
+typedef struct{
+  int dia;
+  int mes;
+  int ano;
+} Data;
+
+
+typedef struct{
+  char nomeCliente[50];
+  char telefoneCliente[15];
+} Cliente;
+
+
+typedef struct{
+  Cliente cliente;
+  char nomeAnimal[50];
+  int especie;
+  Data dataNascimento;
+  char agressivo; //'S' sim 'N' nao
+} Animal;
+
+typedef struct{
+  int identificador;//AUTO_INCREMENT
+  Data dataServico;
+  Animal pet;
+  int tipoServico
+  char pago; //'S' sim 'N' nao
+} Servico;
 
 //Funcs
 
