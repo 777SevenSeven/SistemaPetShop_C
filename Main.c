d5c27a6a1a489b85b1698d663a575e897ccf30e8
André Antônio da Silva Queiroz <andreantoniodasilvaqueiroz@gmail.com>
2023-11-20 14:32:01 -0300

    Update Main.c
    Adicionei o cabeçalho, coloquem o nome completo de vocês e o RA seguindo a formatação

--- Main.c
+++ Main.c
@@ -1,3 +1,10 @@
+/*
+Trabalho Final da Disciplina de Algoritmos 01: Gerenciador de PetShop.
+Turma: EC41A.2023_01.C11 / Data:18/04/2023.
+Nomes:André Antônio da Silva Queiroz /  RA:a2575310
+......Mathias
+......Gabriel
+*/
 #include <math.h>
 #include <string.h>
 #include <stdio.h>
